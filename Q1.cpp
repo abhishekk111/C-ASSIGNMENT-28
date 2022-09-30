6e07698c65001184cb7f95340684bbe37e3bf29b
abhishekk111 <109384287+abhishekk111@users.noreply.github.com>
2022-09-30 16:57:00 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+    int real,img;
+public:
+    Complex(int r, int i)
+    {
+        real=r;
+        img=i;
+    }
+    Complex()
+    {
+
+    }
+    friend ostream & operator<<(ostream &os, Complex &c)
+    {
+        os<<"Real = "<<c.real<<" Img = "<<c.img<<endl;
+        return os;
+    }
+    void setData(int real, int img)
+    {
+        this-> real = real;   // naam agar same hai toh this pointer likhna padega
+        this-> img = img;
+    }
+    Complex & operator=(Complex &c)
+    {
+        this-> real = c.real;
+        this-> img = c.img;
+
+        return *this;
+    }
+};
+int main()
+{
+    Complex c1(2,3);
+    Complex c2;
+    c2.setData(4,5);
+    cout<<c1<<c2;
+
+    return 0;
+
+}
